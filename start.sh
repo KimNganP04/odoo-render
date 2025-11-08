275594902f52bd96517bcae4e7535c079c18482b
KimNganP04 <phamngan15110710@gmail.com>
2025-11-08 20:51:39 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 
-# Chờ DB sẵn sàng
+echo "Waiting for PostgreSQL to be ready..."
 until PGPASSWORD=$PGPASSWORD psql -h "$PGHOST" -U "$PGUSER" -p "$PGPORT" -d postgres -c '\q'; do
-  echo "Waiting for postgres..."
   sleep 2
 done
 
 echo "Database ready! Starting Odoo..."
-
-# Chạy Odoo
-exec odoo -c /etc/odoo/odoo.conf --dev=reload
+exec odoo -c /etc/odoo/odoo.conf
