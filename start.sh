bb8050a2b4ba1ab982f104e7e1c75ea86cf0d24b
KimNganP04 <phamngan15110710@gmail.com>
2025-11-08 21:46:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Waiting for PostgreSQL to be ready..."
-
-# Dùng nc (netcat) thay psql – có sẵn trong image
-until nc -z "$PGHOST" "$PGPORT"; do
-  echo "PostgreSQL is unavailable - sleeping"
-  sleep 2
-done
-
-echo "PostgreSQL is up - starting Odoo..."
+echo "Starting Odoo with DATABASE_URL..."
 exec odoo -c /etc/odoo/odoo.conf
