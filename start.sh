e81fcc2ce5d7ce38c45775b3806bbefdf484ae26
KimNganP04 <phamngan15110710@gmail.com>
2025-11-08 21:57:58 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "Starting Odoo with DATABASE_URL..."
-exec odoo -c /etc/odoo/odoo.conf
+echo "=== STARTING ODOO 19 ==="
+echo "DATABASE_URL: ${DATABASE_URL:0:20}..."
+
+# Chạy Odoo với verbose
+exec odoo -c /etc/odoo/odoo.conf -d postgres --load-language=en_US --stop-after-init || \
+     exec odoo -c /etc/odoo/odoo.conf
